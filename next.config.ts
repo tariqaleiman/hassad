30a88de22bfb7047b675ea2e8f9dcbb86c140e82
Tariq Aleiman <mohamedibrahimhora@gmail.com>
2026-07-14 14:29:33 +0300

    chore: setup github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, "") || "";
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  output: "export",
+  assetPrefix,
+  basePath,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
